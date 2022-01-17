7ac8e975ea7b05f7e27331966e0fc6cc15ef6810
김동규 <tkillman@logiall.com>
2022-01-17 09:48:14 +0900

    first commit

---
+++ src/main/java/com/example/springcircuitbreaker/SpringCircuitbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircuitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCircuitbreakerApplication.class, args);
+	}
+
+}
